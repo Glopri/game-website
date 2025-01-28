9a19ef97fd9252b2cc0b74631b9df46259eaa426
glopri <eliasturpeinen11@gmail.com>
2025-01-28 18:15:56 +0200

    test worked now pushing the rest of what we did this week to github

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 document.getElementById("new-game-btn").addEventListener('click', startGame);
 
 function startGame() {
-    console.log('clicked');
+    document.getElementById("intro-screen").style.display = 'none';
+    document.getElementById("game-screen").style.display = 'block';
 }
