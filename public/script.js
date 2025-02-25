4056e4421bf1ee392b39d9045548c97ebdc88d9e
glopri <eliasturpeinen11@gmail.com>
2025-02-25 18:17:51 +0200

    updating and also adding new obstacles :3

--- public/script.js
+++ public/script.js
@@ -1,36 +1,100 @@
 let BOARD_SIZE = 15;
-let board; //kenttä tallennetaan tähän
+let board; //kenttä talennetaan tähän
+const cellSize = calculateCellSize();
 
 
 document.getElementById("new-game-btn").addEventListener('click', startGame);
 
-function startGame() {
+//JÄÄTIIN TÄHÄN ELI RESPOSIIN
+function calculateCellSize(){
+// Otetaan talteen pienempi luku ikkunan leveydestä ja korkeudesta
+  const screenSize = Math.min(window.innerWidth, window.innerHeight);
+
+  // Tehdään pelilaudasta hieman tätä pienempi, jotta jää pienet reunat
+  const gameBoardSize = 0.95 * screenSize;
+
+  // Laudan koko jaetaan ruutujen määrällä, jolloin saadaan yhden ruudun koko
+  return gameBoardSize / BOARD_SIZE;
+}
+
+function startGame(){
     document.getElementById("intro-screen").style.display = 'none';
     document.getElementById("game-screen").style.display = 'block';
 
     board = generateRandomBoard();
+    drawBoard(board);
 }
 
-function generateRandomBoard() {
-    const newBoard = Array.from({length: BOARD_SIZE}, () =>
-    Array.apply(BOARD_SIZE).fill(' '));
+function getCell(board, x, y) {
+    return board[y][x];
+}
+
+function generateRandomBoard(){
+
+    const newBoard = Array.from({ length: BOARD_SIZE}, () =>
+        Array.apply(BOARD_SIZE).fill(' '));
 
     console.log(newBoard);
+
 // set walls in edges
 for (let y = 0; y < BOARD_SIZE; y++) {
+
     for (let x = 0; x < BOARD_SIZE; x++) {
      if (y === 0 || y === BOARD_SIZE - 1 || x === 0 || x === BOARD_SIZE - 1) {
      newBoard[y][x] = 'W'; //W is wall
      }
     }
+
    }
    
-   
-   
+   generateObstacles(newBoard);
+
    return newBoard;
+
 }
 
-function drawBoard(board){
+function drawBoard(board) {
     const gameBoard = document.getElementById('game-board');
-    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, 1fr)`; //we got here until we had to go. we were setting the grid dynamically by board size
+
+    //Asetataan grid sarakkeet ja rivit dynaamisesti BOARD_SIZEN mukaan
+    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, 1fr)`;
+
+//luodaan jokainen ruutu
+for (let y = 0; y< BOARD_SIZE; y++){
+    for (let x= 0; x < BOARD_SIZE; x++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell'); 
+        cell.style.width = cellSize + "px";
+        cell.style.height = cellSize + "px";
+
+        if (getCell(board, x, y) === 'W') {
+            cell.classList.add('wall')
+        }
+
+        gameBoard.appendChild(cell);
+        
+    }
+}
+
+}
+
+function generateObstacles(board){
+    const obstacles = [
+        [[0,0], [0,1], [1,0], [1,1]] //a square
+    ];
+
+    const positions = [
+        {startX: 5, startY: 7},
+        {startX: 10, startY: 10}
+    ];
+
+    positions.forEach( pos =>{
+        const randomObstacle = obstacles[0];
+
+        for(coordinatePair of randomObstacle){
+            [x,y] = coordinatePair;
+            board [pos.startY + y][pos.startX + x] + "W";
+        }
+    });
+
 }
